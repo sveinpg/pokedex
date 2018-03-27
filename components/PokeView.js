c3ff95cf58c2d9b2d4ef8e40bfec140d3ca1f1b1
sveinpg <sveinpg.gmail.com>
2018-03-27 12:59:29 +0200

    Fetch pokemon information

--- components/PokeView.js
+++ components/PokeView.js
@@ -1,22 +1,53 @@
 import React from "react";
-import { View, Image, Button, StyleSheet } from "react-native";
+import { View, Image, Text, Button, StyleSheet } from "react-native";
 
-const PokeView = ({ id, handleClick }) => {
-  return (
-    <View>
-      <Image
-        source={{
-          uri:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
-            id +
-            ".png"
-        }}
-        style={styles.image}
-      />
-      <Button title="Back" onPress={handleClick} />
-    </View>
-  );
-};
+class PokeView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemon: {}
+    };
+  }
+
+  componentDidMount() {
+    const { id } = this.props;
+    if (id) {
+      fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`)
+        .then(res => res.json())
+        .then(data => {
+          this.setState({ pokemon: data });
+        })
+        .catch(err => console.log(err));
+    }
+  }
+
+  render() {
+    const { id, handleClick } = this.props;
+    const { pokemon } = this.state;
+    return (
+      <View>
+        <Image
+          source={{
+            uri:
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+              id +
+              ".png"
+          }}
+          style={styles.image}
+        />
+        {pokemon &&
+          pokemon.name && <Text>{"ID: " + id + " " + pokemon.name}</Text>}
+        {pokemon &&
+          pokemon.types &&
+          pokemon.types[0].type.name && (
+            <Text>{"Type: " + pokemon.types[0].type.name}</Text>
+          )}
+        <Button title="Back" onPress={handleClick} />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   image: {
